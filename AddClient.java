5fae99938a8c58796c33fb76e885d439afb0ed9f
os <106179721+yourdl@users.noreply.github.com>
2024-04-23 23:55:18 +0530

    Add files via upload

---
+++ AddClient.java
@@ -0,0 +1,69 @@
+import java.rmi.*;
+public class AddClient {
+public static void main(String args[]) {
+try {
+String addServerURL = "rmi://" + args[0] + "/AddServer";
+AddServerIntf addServerIntf =
+(AddServerIntf)Naming.lookup(addServerURL);
+System.out.println("The first number is: " + args[1]);
+double d1 = Double.valueOf(args[1]).doubleValue();
+System.out.println("The second number is: " + args[2]);
+double d2 = Double.valueOf(args[2]).doubleValue();
+System.out.println("The sum is: " + addServerIntf.add(d1, d2));
+}
+catch(Exception e) {
+System.out.println("Exception: " + e); } } }
+
+//AddServer.java:
+import java.net.*;
+import java.rmi.*;
+
+public class AddServer {
+    public static void main(String args[]) {
+        try {
+            AddServerImpl addServerImpl = new AddServerImpl();
+            Naming.rebind("AddServer", addServerImpl);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+}
+
+/*
+ * terminal 1:
+ * javac *.java
+ * rmic AddServerImpl
+ * 
+ * terminal 2:
+ * rmiregistry
+ * 
+ * terminal 3:
+ * java AddServer
+ * 
+ * terminal 4:
+ * java AddClient 127.0.0.1 5 8
+ * or
+ * java AddClient localhost 5 8
+ */
+
+
+//AddServerImpl.java
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class AddServerImpl extends UnicastRemoteObject
+        implements AddServerIntf {
+    public AddServerImpl() throws RemoteException {
+    }
+
+    public double add(double d1, double d2) throws RemoteException {
+        return d1 + d2;
+    }
+}
+
+//AddServerIntf.java
+import java.rmi.*;
+
+public interface AddServerIntf extends Remote {
+    double add(double d1, double d2) throws RemoteException;
+}
